a9c1e1bd4611ea52c9d39cff60ab4bad5dd261c6
d483ulert <tottenham3@naver.com>
2022-10-21 22:21:57 +0900

    Object immutable

---
+++ Object.js
@@ -0,0 +1,16 @@
+
+
+const previousObj ={
+    name: "crong",
+    lastTime: "11:20"  
+};
+
+const myHealth = Object.assign({},previousObj, {
+    "lastTime": "12:30",
+    "name": "honux",
+    "age": 99
+});
+
+console.log(myHealth);
+
+console.log(previousObj);
